11599d6708e3cd7db4435f6e860460840e7e7a36
BeFun44 <fantahun360@gmail.com>
2022-12-08 09:18:47 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,28 @@
+#!/bin/bash
+/*Simple C program that prints an output using puts*/
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  *main - prints a string
+ *
+ *   *
+ *
+ *    * puts appends a trailing new line automatically
+ *
+ *     *Return: 0(Success)
+ *
+ *      */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
