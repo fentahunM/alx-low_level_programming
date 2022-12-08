fed73e2f032bc21da8428d6a05e3487c98b87713
BeFun44 <fantahun360@gmail.com>
2022-12-08 09:44:39 -0800

    Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+lude <stdio.h>
+
+/**
+ *
+ *  * main - prints a string
+ *
+ *   * Return: 0(Success)
+ *
+ *    */
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
