c474fcfd3a9aaaffd7a43dce5bdd7bba75e457ca
BeFun44 <fantahun360@gmail.com>
2022-12-08 10:09:14 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,11 @@
 lude <stdio.h>
-
 /**
- *
- *  * main - prints a string
- *
- *   * Return: 0(Success)
- *
- *    */
-
+ * main - prints a string
+ * Return: 0(Success)
+ */
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 
 }
