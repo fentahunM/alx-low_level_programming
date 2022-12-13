0d61f1560eeb50fd7e03348317d7ff3a9322d1d5
BeFun44 <fantahun360@gmail.com>
2022-12-13 09:03:05 -0800

    task11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_to_98 - prints numbers from n to 98
+ *@n: starting point
+ *
+ *Return: 0
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i == 98)
+			{
+				printf("%d\n", i);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
+		}
+	}
+	else if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i == 98)
+			{
+				printf("%d\n", i);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
+		}
+	}
+	else
+		printf("%d\n", n);
+
+}
